30d7caa12e6ca5189f1aa7f34e220d50bb772ea8
Paradoxon <two4god@gmail.com>
2017-08-09 15:39:10 +0200

    add FontView Class  which will be the main "worker" class

---
+++ FontView.cpp
@@ -0,0 +1,8 @@
+/*
+ * Copyright 2017 Paradoxon
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
+
+#include "FontView.h"
+
---
+++ FontView.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright 2017 Paradoxon
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+#ifndef FONT_VIEW_H
+#define FONT_VIEW_H
+
+
+#include <SupportDefs.h>
+
+
+class FontView {
+public:
+								
+private:
+
+};
+
+
+#endif // FONT_VIEW_H
