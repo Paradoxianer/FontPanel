d53321566f6b467c4a656993f4c1bd7994f970dd
Paradoxon <two4god@gmail.com>
2017-08-16 23:36:57 +0200

    made FonPreview more usefull and  should support basic outline drawing now

--- FontPreview.cpp
+++ FontPreview.cpp
@@ -1,18 +1,34 @@
+#include <stdlib.h>
+
 #include "FontPreview.h"
+#include "FontPanel.h"
 
 
 FontPreview::FontPreview()
  :	BView("fontpreview",B_FOLLOW_LEFT_RIGHT | B_FOLLOW_TOP),
- 	fPreviewText(PREVIEW_STR)
+ 	fPreviewText(PREVIEW_STR),
+ 	fShapes(NULL)
 {
 	SetFlags(B_WILL_DRAW);
+	SetDrawingMode(B_OP_COPY);
+	_RecalcSize();
 }
 
 
 void
 FontPreview::SetPreviewText(const char *text)
 {
-	fPreviewText = text ? text : PREVIEW_STR;
+	if (fPreviewText.Compare(text) != 0) {
+		fPreviewText = text;
+		BFont font;
+		GetFont(&font);
+		if ((font.Face() & B_OUTLINED_FACE) != 0){
+			_AddShapes(fPreviewText);
+			font.GetGlyphShapes(fPreviewText, fPreviewText.CountChars(), fShapes);
+			//if so we need ot get the escapements to calculate it right
+		}
+		_RecalcSize();
+	}
 }
 
 
@@ -24,12 +40,19 @@ FontPreview::PreviewText(void) const
 
 
 void
+FontPreview::SetFont(const BFont* font,uint32 mask)
+{
+	BView::SetFont(font,mask);
+	_RecalcSize();
+}
+
+
+void
 FontPreview::Draw(BRect r)
 {
 	BFont font;
 	GetFont(&font);
-	int32 width = (int32)font.StringWidth(fPreviewText.String());
-	
+	int32 width = (int32)font.StringWidth(fPreviewText.String());	
 	BPoint drawpt;
 	if (width < Bounds().IntegerWidth())
 		drawpt.x = (Bounds().IntegerWidth() - width) / 2;
@@ -38,17 +61,53 @@ FontPreview::Draw(BRect r)
 	
 	font_height fheight;
 	font.GetHeight(&fheight);
-	int32 size = (int32)(fheight.ascent + fheight.descent + fheight.leading);
+	int32 fullHeight = (int32)(fheight.ascent + fheight.descent + fheight.leading);
 	
-	if (size < Bounds().IntegerHeight() - 10)
+	if (fullHeight < Bounds().IntegerHeight() - 10)
 		drawpt.y = (Bounds().IntegerHeight() + fheight.ascent) / 2;
 	else
 		drawpt.y = Bounds().bottom - 10;
-	SetDrawingMode(B_OP_COPY);
+	
 	BRect rect = Bounds();
 	rgb_color hColor = HighColor();
-	SetHighColor(ViewColor());
+	SetHighColor(255,255,255,255);
 	FillRect(rect);
 	SetHighColor(hColor);
-	DrawString(fPreviewText.String(), drawpt);
+	
+	if ((font.Face() & B_OUTLINED_FACE) != 0){
+		for (size_t i = 0; i < fPreviewText.CountChars(); i++) {
+			MovePenTo(drawpt);
+			SetHighColor(255,255,255,255);
+			FillShape(fShapes[i]);
+			SetPenSize(1.5);
+			SetHighColor(0, 0, 0);
+			StrokeShape(fShapes[i]);
+			//**calulate the starting point of the next letter
+		}
+	}
+	else
+		DrawString(fPreviewText.String(), drawpt);
+}
+
+void
+FontPreview::_AddShapes(BString string)
+{
+	const size_t size = string.CountChars();
+	fShapes = (BShape**)malloc(sizeof(BShape*) * size);
+
+	for (size_t i = 0; i < size; i++) {
+		fShapes[i] = new BShape();
+	}
+}
+
+void
+FontPreview::_RecalcSize(void)
+{
+	BFont font;
+	font_height fheight;
+
+	GetFont(&font);
+	font.GetHeight(&fheight);
+	int32 fullHeight = (int32)(fheight.ascent + fheight.descent + fheight.leading+20);
+	SetExplicitMaxSize(BSize(B_SIZE_UNLIMITED, fullHeight));
 }
--- FontPreview.h
+++ FontPreview.h
@@ -1,12 +1,12 @@
 #ifndef FONT_PREVIEW_H_
 #define FONT_PREVIEW_H_
 
-#include <View.h>
+
 #include <Font.h>
 #include <InterfaceDefs.h>
+#include <Shape.h>
 #include <String.h>
-
-#define	PREVIEW_STR "AaBbCcDdEeFfGg!?1234567890"
+#include <View.h>
 
 class FontPreview : public BView
 {
@@ -15,11 +15,18 @@ public:
 	
 	void			SetPreviewText(const char *text);
 	const char *	PreviewText(void) const;
-			
+			 
 	void			Draw(BRect r);
 	
+	//overwrite SetFont and set the new SetExplicitMaxSize
+	void			SetFont(const BFont* font,
+							uint32 mask = B_FONT_ALL);
+	
 private:
-	BString	fPreviewText;
+	void			_AddShapes(BString string);
+	void			_RecalcSize(void);
+	BString			fPreviewText;
+	BShape**		fShapes;
 };
 
 #endif
